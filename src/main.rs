383a65697e94f14c73364970738af5af61a69a59
Sarvarian <Sarvarian@Hotmail.com>
2022-09-05 16:47:51 -0700

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,11 @@ pub use sanitize_root_dir_path::sanitize_root_dir_path;
 fn main() {
     let arg_path = std::env::args().next().unwrap();
     let cli = Cli::parse();
-    let root = sanitize_root_dir_path(&cli.root, arg_path.as_str()).unwrap();
-
+    let root = sanitize_root_dir_path(&cli.root, arg_path.as_str());
+    if root.is_ok() == false {
+        println!("Project Root Directory Path is InValid! Try Again.");
+    }
+    let root = root.unwrap();
     let file_system = FileSystem::new(root.as_path());
 
     println!("{}", file_system.root().display());
