6267bb9110502e32aedab9023caee06c1b58e06b
Sarvarian <Sarvarian@Hotmail.com>
2022-09-05 20:08:26 -0700

    create aban_dot_ab_file.rs

---
+++ src/aban_dot_ab_file.rs
@@ -0,0 +1,79 @@
+use std::{
+    ffi::OsStr,
+    fs::{DirEntry, File},
+    io::{self, Read},
+};
+
+use crate::FileSystem;
+
+/// If [`AbanDotAbFile`].new() Failed.
+pub enum Error {
+    FileNotFind,
+    MultipleFile,
+    OnOpenFile(io::Error),
+    OnReadFile(io::Error),
+}
+
+/// Responsible to load and keep Aban.ab file data.
+pub struct AbanDotAbFile {
+    /// Buffer were keep the Aban.ab file data.
+    data: String,
+    /// Number of bytes were read to data.
+    len: usize,
+}
+
+impl AbanDotAbFile {
+    /// Creates a new [`AbanDotAbFile`].
+    pub fn new(file_system: &FileSystem) -> Result<Self, Error> {
+        let list = Self::aban_dot_ab_list(file_system);
+
+        if list.is_empty() {
+            return Err(Error::FileNotFind);
+        }
+
+        if list.len() > 1 {
+            return Err(Error::MultipleFile);
+        }
+
+        let mut file = match File::open(&list[0].path()) {
+            Ok(file) => file,
+            Err(error) => return Err(Error::OnOpenFile(error)),
+        };
+
+        let mut data = String::new();
+        let len = 0;
+
+        match file.read_to_string(&mut data) {
+            Ok(len) => len,
+            Err(error) => return Err(Error::OnReadFile(error)),
+        };
+
+        Ok(AbanDotAbFile { data, len })
+    }
+
+    /// Returns a reference to the data of this [`AbanDotAbFile`].
+    fn data(&self) -> &str {
+        &self.data
+    }
+
+    /// Returns the length of this [`AbanDotAbFile`].
+    fn len(&self) -> usize {
+        self.len
+    }
+
+    /// List of entries in project root directory with Aban.ab name.
+    /// - case insensitive.
+    fn aban_dot_ab_list(file_system: &FileSystem) -> Vec<DirEntry> {
+        let mut aban_entries = Vec::<DirEntry>::with_capacity(2);
+        let name = OsStr::new("Aban.ab").to_ascii_lowercase();
+
+        for item in file_system.list().unwrap() {
+            let item = item.unwrap();
+            if item.file_name().to_ascii_lowercase() == name {
+                aban_entries.push(item);
+            }
+        }
+
+        aban_entries
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
+mod aban_dot_ab_file;
 mod cli;
 mod file_system;
 mod sanitize_root_dir_path;
 
-use std::{ffi::OsStr, fs::DirEntry};
-
+pub use aban_dot_ab_file::AbanDotAbFile;
 use clap::Parser;
 pub use cli::Cli;
 pub use file_system::FileSystem;
@@ -20,24 +20,4 @@ fn main() {
     let file_system = FileSystem::new(root.as_path());
 
     println!("{}\n", file_system.root().display());
-
-    find_aban_dot_ab(&file_system);
-}
-
-fn find_aban_dot_ab(file_system: &FileSystem) {
-    let mut aban_entries = Vec::<DirEntry>::with_capacity(2);
-    let name = OsStr::new("Aban.ab").to_ascii_lowercase();
-
-    for item in file_system.list().unwrap() {
-        let item = item.unwrap();
-        println!("{:?}", item.file_name());
-        if item.file_name().to_ascii_lowercase() == name {
-            aban_entries.push(item);
-        }
-    }
-
-    println!();
-    for item in aban_entries {
-        println!("{:?}", item);
-    }
 }
