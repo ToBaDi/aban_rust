6b5591a07d819a16e86186e50767c484f87c2d1e
Sarvarian <Sarvarian@Hotmail.com>
2022-09-06 16:16:40 -0700

    Update project_root_dir_path.rs

--- src/project_root_dir_path.rs
+++ src/project_root_dir_path.rs
@@ -66,7 +66,47 @@ fn sanitize_root_dir_path(cli_root: &Option<PathBuf>, arg_path: &str) -> Result<
 mod test {
     use super::sanitize_root_dir_path;
     use super::Error;
+    use super::ProjectRootDirPath;
     use std::path::PathBuf;
+
+    fn get_path_file() -> PathBuf {
+        let args: Vec<String> = std::env::args().collect();
+        let path_file = std::path::Path::new(&args[0]);
+        return path_file.to_owned();
+    }
+
+    fn get_path_dir() -> PathBuf {
+        let path_file = get_path_file();
+        let mut path_file_ancestors = path_file.ancestors();
+        path_file_ancestors.next();
+        let path_dir = path_file_ancestors.next().unwrap();
+        return path_dir.to_owned();
+    }
+
+    #[test]
+    fn on_new_path_passed_correctly() {
+        let path_dir = get_path_dir();
+        assert_eq!(
+            ProjectRootDirPath::new(&None, path_dir.to_str().unwrap()).unwrap(),
+            ProjectRootDirPath(path_dir)
+        );
+
+        let path_dir = get_path_dir();
+        let path_file = get_path_file();
+        assert_eq!(
+            ProjectRootDirPath::new(&None, path_file.to_str().unwrap()).unwrap(),
+            ProjectRootDirPath(path_dir)
+        );
+    }
+
+    #[test]
+    fn on_new_should_fail_on_file_path_passed() {
+        let path = "/aban.ab/aban.ab";
+        assert_eq!(
+            ProjectRootDirPath::new(&None, path),
+            Err(Error::NoDirectory),
+        )
+    }
 
     #[test]
     fn sanitize_root_dir_path_test() {
