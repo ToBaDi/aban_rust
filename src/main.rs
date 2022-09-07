bdd5827b97eefb4ed063657582aedd090dbb7e55
Sarvarian <Sarvarian@Hotmail.com>
2022-09-06 22:01:10 -0700

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,8 @@ use clap::Parser;
 pub use cli::Cli;
 pub use project_root_dir_path::ProjectRootDirPath;
 
+/// For now, main serve as a playground and test-field.
+/// It probably needs rewrite and clarification for final, real use.
 fn main() {
     let root = match ProjectRootDirPath::new(
         &Cli::parse().root,
@@ -23,13 +25,17 @@ fn main() {
 
     println!("Project Root Directory: {}\n", root.path().display());
 
-    let aban_dot_ab = match AbanDotAbFile::new(&root) {
+    let aban = match AbanDotAbFile::new(&root) {
         Ok(aban) => aban,
         Err(error) => {
-            println!("Error from Aban.ab File: {:?}\n", error);
+            println!("Error from opening Aban.ab file: {:?}\n", error);
             return;
         }
     };
 
-    println!("string len {}", aban_dot_ab.data().len());
+    let aban_iter = aban.data().chars();
+
+    for item in aban_iter {
+        println!("{:?}", item);
+    }
 }
