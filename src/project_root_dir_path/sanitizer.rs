4228d450034c35ed25abddcb62a0f95dc99aca0e
Sarvarian <Sarvarian@Hotmail.com>
2022-09-08 17:33:20 -0700

    restructure the file system

--- src/project_root_dir_path/sanitizer.rs
+++ src/project_root_dir_path/sanitizer.rs
@@ -1,44 +1,13 @@
-use std::{
-    fs::{read_dir, ReadDir},
-    io,
-    path::{Path, PathBuf},
-};
-
-#[derive(PartialEq, Debug)]
-pub enum Error {
-    NoDirectory,
-}
-
-/// Responsible for reading file system and
-/// providing file and directory information
-/// to the rest of the application.
-#[derive(Debug, PartialEq)]
-pub struct ProjectRootDirPath(PathBuf);
-
-impl ProjectRootDirPath {
-    /// Creates a new [`FileSystem`].
-    /// - cli_root is suppose fo came from [`crate::Cli`]
-    /// - arg_path is suppose to came from [`std::env::args()`].
-    pub fn new(cli_root: &Option<PathBuf>, arg_path: &str) -> Result<Self, Error> {
-        let path = sanitize_root_dir_path(cli_root, arg_path)?;
-        Ok(ProjectRootDirPath(path))
-    }
-
-    /// Returns a reference to the root of this [`FileSystem`].
-    pub fn path(&self) -> &Path {
-        self.0.as_path()
-    }
-
-    /// List of all the items in [`FileSystem`] root directory.
-    pub fn list(&self) -> io::Result<ReadDir> {
-        read_dir(&self.0)
-    }
-}
+use crate::project_root_dir_path::Error;
+use std::path::PathBuf;
 
 /// Try to make a proper directory path from command-line argument.
 /// - cli_root is suppose fo came from [`crate::Cli`]
 /// - arg_path is suppose to came from [`std::env::args()`].
-fn sanitize_root_dir_path(cli_root: &Option<PathBuf>, arg_path: &str) -> Result<PathBuf, Error> {
+pub fn sanitize_root_dir_path(
+    cli_root: &Option<PathBuf>,
+    arg_path: &str,
+) -> Result<PathBuf, Error> {
     // Check if Cli input is usable as PathBuf.
     // Or turn arg_root path to PathBuf.
     let path = match cli_root {
@@ -64,49 +33,11 @@ fn sanitize_root_dir_path(cli_root: &Option<PathBuf>, arg_path: &str) -> Result<
 
 #[cfg(test)]
 mod test {
-    use super::sanitize_root_dir_path;
-    use super::Error;
-    use super::ProjectRootDirPath;
     use std::path::PathBuf;
 
-    fn get_path_file() -> PathBuf {
-        let args: Vec<String> = std::env::args().collect();
-        let path_file = std::path::Path::new(&args[0]);
-        return path_file.to_owned();
-    }
+    use crate::project_root_dir_path::Error;
 
-    fn get_path_dir() -> PathBuf {
-        let path_file = get_path_file();
-        let mut path_file_ancestors = path_file.ancestors();
-        path_file_ancestors.next();
-        let path_dir = path_file_ancestors.next().unwrap();
-        return path_dir.to_owned();
-    }
-
-    #[test]
-    fn on_new_path_passed_correctly() {
-        let path_dir = get_path_dir();
-        assert_eq!(
-            ProjectRootDirPath::new(&None, path_dir.to_str().unwrap()).unwrap(),
-            ProjectRootDirPath(path_dir)
-        );
-
-        let path_dir = get_path_dir();
-        let path_file = get_path_file();
-        assert_eq!(
-            ProjectRootDirPath::new(&None, path_file.to_str().unwrap()).unwrap(),
-            ProjectRootDirPath(path_dir)
-        );
-    }
-
-    #[test]
-    fn on_new_should_fail_on_file_path_passed() {
-        let path = "/aban.ab/aban.ab";
-        assert_eq!(
-            ProjectRootDirPath::new(&None, path),
-            Err(Error::NoDirectory),
-        )
-    }
+    use super::sanitize_root_dir_path;
 
     #[test]
     fn sanitize_root_dir_path_test() {
